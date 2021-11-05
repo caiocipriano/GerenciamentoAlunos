bade8a306f6894bc3a90fbf01843048bed1322ec
Caio Cipriano <caio.silvax@hotmail.com>
2021-11-05 09:44:30 -0300

    Alteração 05/11

--- src/main/java/br/com/academy/controllers/AlunoController.java
+++ src/main/java/br/com/academy/controllers/AlunoController.java
@@ -1,7 +1,10 @@
 package br.com.academy.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,28 +18,33 @@ public class AlunoController {
 		
 	
 	@Autowired
-	private AlunoDao alunoRepositorio;
+	private AlunoDao alunoRepositorio;	//Anotação JPA
 	
 	
 	@GetMapping("/inserir")
-	public ModelAndView InsertAlunos(Aluno aluno) {
+	public ModelAndView InsertAlunos(Aluno aluno) {	//Metodo vinculado ao Objeto aluno 
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("Aluno/formAluno");
-		mv.addObject("aluno",new Aluno());
+		mv.setViewName("Aluno/formAluno");		//Assim que inserir, vai retornar a html formAluno
+		mv.addObject("aluno",new Aluno());		//Vincula ao Objeto Aluno	
 		return mv;
 	}
 	
 	@PostMapping("/IstAlunos")
-	public ModelAndView inserirAluno(Aluno aluno) {
+	public ModelAndView inserirAluno(@Valid Aluno aluno, BindingResult br ) {
 		ModelAndView mv = new ModelAndView();
+		if(br.hasErrors()) {
+			mv.setViewName("Aluno/formAluno"); //Mantém o usuário na página de formulario
+			mv.addObject(aluno);
+		}else {
 		mv.setViewName("redirect:/alunos-adicionados");
-		alunoRepositorio.save(aluno);
+		alunoRepositorio.save(aluno);	// O método inserirAluno vai inserir no banco atráves do JPA
+		}                        
 		return mv;	
 	}
 	
 	@GetMapping("alunos-adicionados")
 	public ModelAndView listagemAlunos(){
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView();			//Método que vai retorna a View onde contém a lista de alunos
 		mv.setViewName("Aluno/listAlunos");
 		mv.addObject("alunosList", alunoRepositorio.findAll());
 		return mv;
@@ -44,24 +52,24 @@ public class AlunoController {
 	}
 	
 	@GetMapping("/alterar/{id}")
-	public ModelAndView alterar(@PathVariable("id") Integer id) {
+	public ModelAndView alterar(@PathVariable("id") Integer id) { //Path indentica o ID
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("Aluno/alterar");	
-		Aluno aluno = alunoRepositorio.getOne(id);
+		mv.setViewName("Aluno/alterar");						 //Altera os dados apontando para o ID
+		Aluno aluno = alunoRepositorio.getOne(id);	        	
 		mv.addObject("aluno",aluno);
 		return mv;
 	}
 	
 	@PostMapping("/alterar")
-	public ModelAndView alterar(Aluno aluno) {
-		ModelAndView mv = new ModelAndView();
+	public ModelAndView alterar(Aluno aluno) { 
+		ModelAndView mv = new ModelAndView();		
 		alunoRepositorio.save(aluno);
-		mv.setViewName("redirect:/alunos-adicionados");
+		mv.setViewName("redirect:/alunos-adicionados"); // alterar() vai redirecionar para página de cadastro
 		return mv;
 	}
 	
 	@GetMapping("/excluir/{id}")
-	public String excluirAluno(@PathVariable("id") Integer id) {
+	public String excluirAluno(@PathVariable("id") Integer id) { //Método para excluir, ele percorre pelo ID no banco
 		alunoRepositorio.deleteById(id);
 		return "redirect:/alunos-adicionados";
 	}
--- src/main/java/br/com/academy/model/Aluno.java
+++ src/main/java/br/com/academy/model/Aluno.java
@@ -1,15 +1,19 @@
 
   package br.com.academy.model;
   
-  import javax.persistence.Column; 
-  import javax.persistence.Entity;
+import javax.persistence.Column; 
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
-  import javax.persistence.GenerationType;
-  import javax.persistence.Id;
-  
-  import br.com.academy.enums.Curso; import br.com.academy.enums.Status;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import br.com.academy.enums.Curso; 
+import br.com.academy.enums.Status;
   
   
   //Anotação JPA para apontar e persistir um objeto, Aluno
@@ -23,21 +27,29 @@ import javax.persistence.GeneratedValue;
 	 private Integer id;
   
 	 @Column(name="nome") 
+	 @Size(min = 5, max = 60, message = "O nome deve no mínimo 5 caracteres")
+	 @NotBlank(message = "O campo nome não pode ser vazio")
 	 private String nome;
 		  
 	 @Column(name="curso") 
 	 @Enumerated(EnumType.STRING)
+	 @NotNull(message="O campo curso não pode ser nulo")
 	 private Curso curso;
 		  
 	 @Column(name="matricula") 
+	 @NotNull(message = "Clique no botão gerar!")
+	 @Size(min = 3, message = "Clique no botão gerar!")
 	 private String matricula;
 		  
 	 @Column(name="status")
 	 @Enumerated(EnumType.STRING)
+	 @NotNull(message="O campo status não pode ser nulo")
 	 private Status status;
 	
 	 @Column(name="turno")
-	 private String turno;
+	 @Size(min = 3,  message = "O campo deve no mínimo 3 caracteres")
+	 @NotBlank(message = "O turno não pode ser vazio")
+	 private String turno;	
 
 	public Integer getId() {
 		return id;
